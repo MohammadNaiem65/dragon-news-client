2f4e2ed17935ae6093088df764cf457f131759aa
MohammadNaiem65 <mohammadnaiem661065@gmail.comm>
2023-06-06 12:19:58 +0600

    completed the man navbar

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -2,7 +2,11 @@ import moment from "moment/moment";
 import logo from "../assets/logo.png";
 import Button from "react-bootstrap/Button";
 import Marquee from "react-fast-marquee";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 import "./Header.css";
+import avatar from "../assets/avater.png";
 
 const Header = () => {
 	return (
@@ -21,11 +25,26 @@ const Header = () => {
 				<Button variant='danger' className='rounded-0'>
 					Latest
 				</Button>
-				<Marquee pauseOnHover='true'>
+				<Marquee pauseOnHover='true' className='ms-3'>
 					I can be a React component, multiple React components, or just some
 					text.
 				</Marquee>
 			</div>
+			<Navbar className='mt-5 mb-3'>
+				<Container>
+					<Nav className='justify-content-end flex-grow-1'>
+						<Nav.Link href='#action1'>Home</Nav.Link>
+						<Nav.Link href='#action2'>About</Nav.Link>
+						<Nav.Link href='#action2'>Career</Nav.Link>
+					</Nav>
+					<Navbar.Collapse className='justify-content-end'>
+						<Nav>
+							<img src={avatar} alt='avatar image' className='avatar' />
+							<Button variant='secondary'>Secondary</Button>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
 		</div>
 	);
 };
